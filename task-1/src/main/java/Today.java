9c33869081332102cf67f25e14d0c873b325783b
Eugene <zohan1234567@gmail.com>
2019-06-28 01:14:06 +0300

    task-1: completed

--- task-1/src/main/java/Today.java
+++ task-1/src/main/java/Today.java
@@ -14,10 +14,10 @@ public class Today {
 
         Date datePlusWeek = new Date();
         SimpleDateFormat formatForDateLater = new SimpleDateFormat("dd MMMM ss HH yyyy mm", getDefault());
-        Calendar c = Calendar.getInstance();
-        c.setTime(datePlusWeek);
-        c.add(Calendar.DAY_OF_WEEK, 7);
-        datePlusWeek = c.getTime();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(datePlusWeek);
+        calendar.add(Calendar.DAY_OF_WEEK, 7);
+        datePlusWeek = calendar.getTime();
         System.out.println( "Hello world + 1 week: " + formatForDateLater.format(datePlusWeek));
         }
 
