a0d8deff8d66d504527b8819ebf01e5636d95509
EugeneZakhno <zohan1234567@gmail.com>
2019-11-29 22:49:26 +0200

    refactoring NPE

--- task-3/src/main/java/by/epam/training/npe_example/ExampleOfNPE.java
+++ task-3/src/main/java/by/epam/training/npe_example/ExampleOfNPE.java
@@ -4,14 +4,13 @@ public class ExampleOfNPE {
 
     public static void main(String[] args) {
 
-        String name;
+        String name= null;
 
         // 1. Когда возникает ?
         //NPE возникает когда мы хотим вызвать метод через ссылку (переменную) иницилизированную как NULL;
 
-
         // Это костыль
-
+        if(name!=null)
         System.out.println(name.length());
 
         // 2. Как бороться?
